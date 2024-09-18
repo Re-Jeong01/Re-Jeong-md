fc645b0bb4337ebd79381916cdd1c7425b23cf9f
Re-Jeong01 <rejeongguanxii@gmail.com>
2024-09-18 10:17:18 +0200

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -1431,62 +1431,33 @@ break;
     } 
   
 break
-  case 'play':
-    case 'stream': {
-     if (!text) {
-            m.reply('What song do you want to download?')
-            return;
-        }
-        try {
-            const {
-                videos
-            } = await yts(text);
-            if (!videos || videos.length <= 0) {
-                reply(`No Matching videos found for : *${args[0]}*!!`)
-                return;
-            }
-            let urlYt = videos[0].url
-            let infoYt = await ytdl.getInfo(urlYt);
-
-
-            const getRandonm = (ext) => {
-                return `${Math.floor(Math.random() * 10000)}${ext}`;
-            };
-            let titleYt = infoYt.videoDetails.title;
-            let randomName = getRandonm(".mp3");
-            const stream = ytdl(urlYt, {
-                    filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
-                })
-                .pipe(fs.createWriteStream(`./${randomName}`));
-            console.log("Audio downloading ->", urlYt);
-            // reply("Downloading.. This may take upto 5 min!");
-            await new Promise((resolve, reject) => {
-                stream.on("error", reject);
-                stream.on("finish", resolve);
-            });
-
-            let stats = fs.statSync(`./${randomName}`);
-            let fileSizeInBytes = stats.size;
-            // Convert the file size to megabytes (optional)
-            let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
-            console.log("Audio downloaded ! \n Size: " + fileSizeInMegabytes);
-
-
-                await client.sendMessage(
-                    m.chat, {
-                        document: fs.readFileSync(`./${randomName}`),
-                        mimetype: "audio/mpeg",
-                        fileName: titleYt + ".mp3",
-                    }, {
-                        quoted: m
-                    }
-                );
-
-            fs.unlinkSync(`./${randomName}`);
-        } catch (e) {
-            m.reply(e.toString())
-        }
-    }
+  case 'play': {
+			if (!text) return zreply(`*Example :*\n\n*Play Mendua*`)
+			zreply(mess.wait);
+			let yts = require("youtube-yts")
+			let look = await yts(text);
+			let convert = look.videos[0];
+			const pl = await youtube(convert.url)
+			await VxoZap.sendMessage(m.chat, {
+				audio: {
+					url: pl.mp3
+				},
+				fileName: convert.title + '.mp3',
+				mimetype: 'audio/mpeg',
+				contextInfo: {
+					externalAdReply: {
+						title: convert.title,
+						body: packname,
+						thumbnailUrl: convert.image,
+						sourceUrl: pl.mp3,
+						mediaType: 1,
+						mediaUrl: convert.url,
+					}
+				},
+			}, {
+				quoted: Zets
+			})
+  }
 break;
 
 case 'ytsearch':
